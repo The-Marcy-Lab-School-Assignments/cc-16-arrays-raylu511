274ae709b88944983f2901e5354d513ab3085bad
Ray Lu <raylu511@gmail.com>
2021-11-16 15:11:59 +0000

    Finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //write your code here
+function replaceWithYerr(array, startIndex = 0, numOfElements = array.length) {
+    const yerrArray = [...array];
+    if (startIndex === 0 && numOfElements === array.length) {
+        for (let i = 0; i < array.length; i++) {
+            yerrArray[i] = 'yerr';
+        }
+    }
+    else if (numOfElements === array.length) {
+        for (let i = startIndex; i < array.length; i++) {
+            yerrArray[i] = 'yerr';
+        }
+    }
+    else {
+        for (let i = startIndex; i < numOfElements + startIndex; i++) {
+            yerrArray[i] = 'yerr';
+        }
+    }
+    return yerrArray;
+}
+
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];    
+console.log(replaceWithYerr(months, 1, 3));     //['Jan', 'yerr', 'yerr, 'yerr', 'May', 'June']
+console.log(replaceWithYerr(months, 1));        //['Jan', 'yerr', 'yerr, 'yerr', 'yerr', 'yerr']
+console.log(replaceWithYerr(months));           //['yerr', 'yerr', 'yerr, 'yerr', 'yerr', 'yerr']
